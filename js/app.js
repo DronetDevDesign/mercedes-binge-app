1b20ec2dbcade79a1e96a97e14f097c91a6bc13b
Ron Dronet <ron@dronetdevdesign.com>
2023-03-08 12:40:26 -0600

    refacting javascrip code for UnSplash API and intergrating css styles and popup styles

--- js/app.js
+++ js/app.js
@@ -42,3 +42,86 @@ function nextTheme() {
   if (counter === themeImage.length) counter = 0;
   return themeImage.setAttribute("src", "images/" + images[counter]);
 }
+
+// UnSplash API keys
+const access_key = "s_HHGrKfzFsV6A9Xu3kXlnJeaFOzy0p5n4GRc4poxK8";
+const secret_key = "Ii6FESYKTEmvlFZOTPx0S_KYye7-pJDbORBR4vHtBC0";
+
+// UnSplash image access
+const mercedes_url = `https://api.unsplash.com/photos/random?client_id=${access_key}&query=mercedes&orientation=portrait&count=12`;
+
+
+const gallery = document.querySelector(".gallery");
+
+let currentImage = 0;
+let allImages; // this will store all the images
+
+const getImages = () => {
+  fetch(mercedes_url)
+    .then((res) => res.json())
+    .then((data) => {
+      allImages = data;
+      makeImages(allImages);
+    });
+};
+
+const makeImages = (data) => {
+  data.forEach((item, index) => {
+    let img = document.createElement("img");
+    img.src = item.urls.small;
+    img.className = "gallery-img"; // this class should be in the HTML document
+
+    gallery.appendChild(img);
+
+    // popup image
+
+    img.addEventListener("click", () => {
+      currentImage = index;
+      showPopup(item);
+    });
+  });
+};
+
+const showPopup = (item) => {
+  let popup = document.querySelector(".img-popup");
+  const downloadBtn = document.querySelector(".download-btn");
+  const closeBtn = document.querySelector(".close-btn");
+  const image = document.querySelector(".large-img");
+
+  popup.classList.remove("hide");
+  downloadBtn.href = item.links.html;
+  image.src = item.urls.regular;
+
+  closeBtn.addEventListener("click", () => {
+    popup.classList.add("hide");
+  });
+};
+
+getImages();
+
+// controls
+
+const preBtn = document.querySelector(".pre-btn");
+const nxtBtn = document.querySelector(".nxt-btn");
+
+preBtn.addEventListener("click", () => {
+  if (currentImage > 0) {
+    currentImage--;
+    showPopup(allImages[currentImage]);
+  }
+});
+
+nxtBtn.addEventListener("click", () => {
+  if (currentImage < allImages.length - 1) {
+    currentImage++;
+    showPopup(allImages[currentImage]);
+  }
+});
+
+const nxtImg = document.querySelector(".next-images");
+
+nxtImg.addEventListener("click", () => {
+
+  return getImages();
+  // window.location.reload();
+});
